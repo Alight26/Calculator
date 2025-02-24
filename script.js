f3aa59787654d1457f54dfb14f2adbf840c8dfbc
Lucas <luc.coulson6@gmail.com>
2025-02-24 15:43:29 -0500

    Made the divs for the calculator

--- script.js
+++ script.js
@@ -1,54 +1,4 @@
-// math operator functions
-function addition() {
-    let add1 = prompt("enter first number ");
-    add1 = +add1;
-    let add2 = prompt("enter second number ");
-    add2 =+add2;
-    console.log(add1 + add2);
-};
-// addition();
-
-
-function subtract() {
-    let sub1 = prompt("sub1 ");
-    sub1 = +sub1;
-    let sub2 = prompt("sub2 ");
-    sub2 = +sub2;
-
-    console.log(sub1 - sub2);
-};
-
-
-
-
-
-
-function multiply() {
-    let multi1 = prompt("multiply ");
-    multi1 = +multi1;
-    let multi2 = prompt("multiply ");
-    multi2 = +multi2;
-
-    console.log(multi1 * multi2)
-};
-
-
-
-
-
-function divide() {
-    let div1 = prompt("divide ");
-    div1 = +div1;
-    let div2 = prompt("divide ");
-    div2 = +div2;
-
-    console.log(div1 / div2);
-};
-
-
-
-
-
+// calls the math operator functions to perform the specific operation
 function operate() {
     let operator = prompt("boop ");
 
@@ -64,5 +14,45 @@ function operate() {
 }
 
 operate();
+
+// math operator functions
+function addition() {
+    let add1 = prompt("enter first number ");
+    add1 = +add1;
+    let add2 = prompt("enter second number ");
+    add2 =+add2;
+    console.log(add1 + add2);
+};
+
+function subtract() {
+    let sub1 = prompt("sub1 ");
+    sub1 = +sub1;
+    let sub2 = prompt("sub2 ");
+    sub2 = +sub2;
+
+    console.log(sub1 - sub2);
+};
+
+function multiply() {
+    let multi1 = prompt("multiply ");
+    multi1 = +multi1;
+    let multi2 = prompt("multiply ");
+    multi2 = +multi2;
+
+    console.log(multi1 * multi2);
+};
+
+function divide() {
+    let div1 = prompt("divide ");
+    div1 = +div1;
+    let div2 = prompt("divide ");
+    div2 = +div2;
+
+    console.log(div1 / div2);
+};
+
+
+
+
 
 
