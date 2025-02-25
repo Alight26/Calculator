55b66f96a0f1409cee3fd94e4d647f171694732b
Lucas <luc.coulson6@gmail.com>
2025-02-25 13:42:50 -0500

    I need to fork and restart

--- script.js
+++ script.js
@@ -1,59 +1,106 @@
 //numArray
+let firstNumber;
 let firstNumArr = [];
 //number button logic
 function start(){
-    const numbers = document.querySelectorAll('#buttonNumbers button');
-    const operatorBtn = document.querySelectorAll('.op'); 
+    let numbers = document.querySelectorAll('#buttonNumbers button');
+     
     numbers.forEach((button) => {
         button.addEventListener("click", () => {
             firstNumArr.push(button.textContent);
-            console.log(firstNumArr);
-
-            const wholeNumber = firstNumArr.join('');
-            console.log(wholeNumber);
-            return parseInt(wholeNumber);
-        }); 
-    });
-    operatorBtn.forEach((button)=> {
-        button.addEventListener("click", (event) => {
-            const operatorClass = event.target.class;
             
-            if (operatorClass === 'op') {
-                operatorFunction();
-                
-            }
-        })
-    })
 
-    
+            let wholeNumber = firstNumArr.join('');
+            
+            firstNumber = parseInt(wholeNumber);
+            console.log(firstNumber);
+            
+        }); 
+    })
+    let operatorBtn = document.querySelectorAll('#buttonOperators button');
+    operatorBtn.forEach((button) => {
+        button.addEventListener("click", (event) => {     // operator buttons 
+            let operatorId = event.target.id;
+
+            if (operatorId === 'divide') {
+                divide(firstNumber);
+            } else if (operatorId === 'multiply') {
+                multiply(firstNumber);
+            } else if (operatorId === 'add') {
+                addition(firstNumber);
+                console.log("+");
+            } else if (operatorId === 'subtract') {
+                subtract(firstNumber);
+            } else if (operatorId === 'equal') {
+                equal(firstNumber);
+            }
+            
+        })
+    });
 
 };
 
 start();
 
 
-function operatorFunction(){
-    const operatorBtn = document.querySelectorAll('.op'); 
-    operatorBtn.forEach((button) => {
-        button.addEventListener("click", (event) => {     // operator buttons 
-            const operatorId = event.target.id;
 
-            if (operatorId === 'divide') {
-                divide(start());
-            } else if (operatorId === 'multiply') {
-                multiply(start());
-            } else if (operatorId === 'add') {
-                addition(start());
-            } else if (operatorId === 'subtract') {
-                subtract(start());
-            } else if (operatorId === 'equal') {
-                equal(start());
-            }
-            
-        })
-    });
+function addition(firstNumber) {
+    secondNumber();
     
 };
+
+let nextNumber;
+let nextNumArr = [];
+
+function secondNumber(){
+    let newNumbers = document.querySelectorAll('#buttonNumbers button');
+
+    newNumbers.forEach((button) => {
+        button.addEventListener("click", () => {
+            nextNumArr.push(button.textContent);
+            console.log(nextNumArr);
+
+            nextNumber = nextNumArr.join('');
+            
+            nextNumber = parseInt(nextNumArr);
+            console.log(nextNumber);
+            return nextNumber
+            
+
+            
+        }); 
+    });
+};
+
+
+function subtract(firstNum) {
+    let sub1 = prompt("sub1 ");
+    sub1 = +sub1;
+    let sub2 = prompt("sub2 ");
+    sub2 = +sub2;
+
+    console.log(sub1 - sub2);
+};
+
+function multiply(firstNum) {
+    let multi1 = prompt("multiply ");
+    multi1 = +multi1;
+    let multi2 = prompt("multiply ");
+    multi2 = +multi2;
+
+    console.log(multi1 * multi2);
+};
+
+function divide(firstNum) {
+    let div1 = prompt("divide ");
+    div1 = +div1;
+    let div2 = prompt("divide ");
+    div2 = +div2;
+
+    console.log(div1 / div2);
+};
+
+
  
 
 
